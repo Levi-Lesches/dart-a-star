6adcf307e4bcc14b1ff9036e50f2c534bde0e812
Oliver Sand <oliver.sand@tentaclelabs.com>
2014-07-06 20:24:59 +0200

    The method was renamed

--- test/test.dart
+++ test/test.dart
@@ -6,9 +6,9 @@ class SimpleNode extends Object with Node {
   num nodeInherentCost;
 
   SimpleNode(this.nodeInherentCost);
-  
+
   Iterable<SimpleNode> connectedNodes;
-  
+
   num getCostFrom(SimpleNode other) =>
       nodeInherentCost;  // This is how it works in many games - the node itself
                          // has a cost. For example, a forrest has 2x the cost
@@ -16,11 +16,11 @@ class SimpleNode extends Object with Node {
 }
 
 class SimpleNodeNetwork extends Graph<SimpleNode> {
-  
+
   Iterable<SimpleNode> allNodes; // TODO implement this getter
-  
+
   SimpleNode start, a, b, c, d, goal;
-  
+
   SimpleNodeNetwork() {
     // The testing network is set up as follows:
     //      start
@@ -39,14 +39,14 @@ class SimpleNodeNetwork extends Graph<SimpleNode> {
     d = new SimpleNode(2);
     goal = new SimpleNode(3);
 
-    
+
     start.connectedNodes = [a,b];
     a.connectedNodes = [start, b, c, d];
     b.connectedNodes = [start, a, c, d];
     c.connectedNodes = [a, b, d, goal];
     d.connectedNodes = [a, b, c, goal];
     goal.connectedNodes = [c, d];
-    
+
     allNodes = [start, a, b, c, d, goal];
   }
 
@@ -72,25 +72,25 @@ main() {
   group("A* star generic", () {
     SimpleNodeNetwork network;
     AStar<SimpleNode> aStar;
-    
+
     setUp(() {
       network = new SimpleNodeNetwork();
       aStar = new AStar<SimpleNode>(network);
     });
-    
+
     test("finds fastest way", () {
       var path = aStar.findPathSync(network.start, network.goal);
       expect(path, orderedEquals(
           [network.start, network.b, network.c, network.goal]));
     });
-    
+
     test("doesn't find if blocked by impassable", () {
       network.c.nodeInherentCost = null;
       network.d.nodeInherentCost = null;
       var path = aStar.findPathSync(network.start, network.goal);
       expect(path, isEmpty);
     });
-    
+
     test("works for successive path finding on the same AStar instance", () {
       var path = aStar.findPathSync(network.start, network.goal);
       expect(path, orderedEquals(
@@ -99,10 +99,10 @@ main() {
       expect(path2, orderedEquals(
           [network.a, network.c, network.goal]));
     });
-    
+
     test("works asynchronously", () {
       var pathFuture = aStar.findPath(network.start, network.goal);
-      pathFuture.then(expectAsync1((path) {
+      pathFuture.then(expectAsync((path) {
         expect(path, orderedEquals(
             [network.start, network.b, network.c, network.goal]));
       }));
