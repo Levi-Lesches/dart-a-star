32f842ca6db1d007c7abefc1fb30df6ad2f91d9b
Eric Seidel <eric@shorebird.dev>
2024-11-03 16:45:39 -0800

    chore: update to modern Dart style to appease pub

--- lib/src/algorithm.dart
+++ lib/src/algorithm.dart
@@ -1,17 +1,22 @@
 // ignore_for_file: avoid_print
 
 import "package:collection/collection.dart";
+
 import "node.dart";
 import "state.dart";
 
-/// Runs the A* algorithm on the given state, returning the first goal state, or null. 
-/// 
-/// If [verbose] is true, this will print debug info about which states were expanded during the 
-/// search. Once the algorithm reaches [limit] states (1,000 by default), it returns null to 
+/// Runs the A* algorithm on the given state, returning the first goal state, or null.
+///
+/// If [verbose] is true, this will print debug info about which states were expanded during the
+/// search. Once the algorithm reaches [limit] states (1,000 by default), it returns null to
 /// indicate failure.
-/// 
+///
 /// To replay the path from the [state] to the goal state, use [AStarNode.reconstructPath].
-AStarNode<T>? aStar<T extends AStarState<T>>(T state, {bool verbose = false, int limit = 1000}) {
+AStarNode<T>? aStar<T extends AStarState<T>>(
+  T state, {
+  bool verbose = false,
+  int limit = 1000,
+}) {
   final startNode = AStarNode(state);
   final opened = <AStarNode<T>>{startNode};
   final closed = <AStarNode<T>>{};
@@ -20,7 +25,11 @@ AStarNode<T>? aStar<T extends AStarState<T>>(T state, {bool verbose = false, int
 
   while (open.isNotEmpty) {
     final node = open.removeFirst();
-    if (verbose) print("[$count] Exploring: ${node.hash} (${node.depth} + ${node.heuristic} = ${node.cost})");
+    if (verbose) {
+      print(
+        "[$count] Exploring: ${node.hash} (${node.depth} + ${node.heuristic} = ${node.cost})",
+      );
+    }
     if (node.state.isGoal()) return node;
     opened.remove(node);
     closed.add(node);
@@ -30,7 +39,9 @@ AStarNode<T>? aStar<T extends AStarState<T>>(T state, {bool verbose = false, int
     }
     for (final newNode in node.expand()) {
       if (closed.contains(newNode) || opened.contains(newNode)) continue;
-      if (verbose) print("[$count]   Got: ${newNode.hash} (cost = ${newNode.heuristic})");
+      if (verbose) {
+        print("[$count]   Got: ${newNode.hash} (cost = ${newNode.heuristic})");
+      }
       open.add(newNode);
       opened.add(newNode);
     }
--- test/grid_test.dart
+++ test/grid_test.dart
@@ -1,6 +1,7 @@
 import "dart:math";
-import "package:test/test.dart";
+
 import "package:a_star/a_star.dart";
+import "package:test/test.dart";
 
 class CoordinatesState extends AStarState<CoordinatesState> {
   final int x;
@@ -8,23 +9,29 @@ class CoordinatesState extends AStarState<CoordinatesState> {
   final int goalX;
   final int goalY;
   final String? direction;
-  CoordinatesState(this.x, this.y, this.goalX, this.goalY, {this.direction, super.depth = 0});
+  CoordinatesState(
+    this.x,
+    this.y,
+    this.goalX,
+    this.goalY, {
+    this.direction,
+    super.depth = 0,
+  });
 
   Iterable<CoordinatesState> getNeighbors() => [
-    CoordinatesState(x, y + 1, goalX, goalY, direction: "up"),
-    CoordinatesState(x, y - 1, goalX, goalY, direction: "down"),
-    CoordinatesState(x + 1, y, goalX, goalY, direction: "right"),
-    CoordinatesState(x - 1, y, goalX, goalY, direction: "left"),
-  ];
+        CoordinatesState(x, y + 1, goalX, goalY, direction: "up"),
+        CoordinatesState(x, y - 1, goalX, goalY, direction: "down"),
+        CoordinatesState(x + 1, y, goalX, goalY, direction: "right"),
+        CoordinatesState(x - 1, y, goalX, goalY, direction: "left"),
+      ];
 
   bool get isValid => true;
 
   @override
   Iterable<CoordinatesState> expand() => [
-    for (final neighbor in getNeighbors())
-      if (neighbor.isValid)
-        neighbor,
-  ];
+        for (final neighbor in getNeighbors())
+          if (neighbor.isValid) neighbor,
+      ];
 
   @override
   double heuristic() => sqrt(pow(goalX - x, 2) + pow(goalY - y, 2));
@@ -37,16 +44,17 @@ class CoordinatesState extends AStarState<CoordinatesState> {
 }
 
 void main() => test("Simple path should be exactly 21 spaces", () {
-  final start = CoordinatesState(0, 0, 10, 10);
-  final result = aStar(start);
-  expect(result, isNotNull); if (result == null) return;
-  final path = result.reconstructPath();
-  expect(path, isNotEmpty);
-  expect(path, hasLength(21));
-  final origin = path.first;
-  final destination = path.last;
-  expect(origin.x, start.x);
-  expect(origin.y, start.y);
-  expect(destination.x, start.goalX);
-  expect(destination.y, start.goalY);
-});
+      final start = CoordinatesState(0, 0, 10, 10);
+      final result = aStar(start);
+      expect(result, isNotNull);
+      if (result == null) return;
+      final path = result.reconstructPath();
+      expect(path, isNotEmpty);
+      expect(path, hasLength(21));
+      final origin = path.first;
+      final destination = path.last;
+      expect(origin.x, start.x);
+      expect(origin.y, start.y);
+      expect(destination.x, start.goalX);
+      expect(destination.y, start.goalY);
+    });
